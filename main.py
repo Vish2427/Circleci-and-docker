3511820952b5ce5586b1b3dc46c7391b6f2f1b49
vishal <ranbirrathore150@gmail.com>
2022-07-16 14:52:24 +0530

    Circlci test

---
+++ main.py
@@ -0,0 +1,22 @@
+from wsgiref import simple_server
+from flask import Flask, request, render_template
+from flask import Response
+import os
+from flask_cors import CORS, cross_origin
+import flask_monitoringdashboard as dashboard
+import json
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return "This app is running"
+
+
+port = int(os.getenv("PORT", 5000))
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    # port = 5000
+    httpd = simple_server.make_server(host, port, app)
+    # print("Serving on %s %d" % (host, port))
+    httpd.serve_forever()
